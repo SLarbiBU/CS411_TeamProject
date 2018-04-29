800c96276af3e74656ca865aaf294796abea204b
Matt Cote <mwcote97@gmail.com>
2018-04-28 23:26:35 -0400

    fixed data passing between event search and event details

--- myapp/angular/src/app/Services/event-data.service.ts
+++ myapp/angular/src/app/Services/event-data.service.ts
@@ -8,12 +8,30 @@ import {Event} from '../Event';
 export class EventDataService {
 
   private eventSource = new BehaviorSubject<Event>(null);
+  private eventArraySource = new BehaviorSubject<Event[]>([]);
+  private eventLocationSource =new BehaviorSubject<string>("");
+  private eventTermSource = new BehaviorSubject<string>("");
   currentEvent = this.eventSource.asObservable();
+  currentEventArray = this.eventArraySource.asObservable();
+  currentLocation = this.eventLocationSource.asObservable();
+  currentTerm = this.eventTermSource.asObservable();
 
   constructor() { }
 
   changeEvent(event: Event) {
     this.eventSource.next(event)
+  }
+
+  changeEventArray(eventArray: Event[]){
+    this.eventArraySource.next(eventArray);
+  }
+
+  changeLocation(location: string) {
+    this.eventLocationSource.next(location)
+  }
+
+  changeTerm(term: string) {
+    this.eventTermSource.next(term)
   }
 
 }
