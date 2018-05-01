a522fce9aa0cabd110d07bf9533cd26858e9b697
alebrebs <alebrebs@bu.edu>
2018-05-01 07:30:58 -0400

    Update uber.component.ts

--- myapp/angular/src/app/Components/uber/uber.component.ts
+++ myapp/angular/src/app/Components/uber/uber.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import {UberService} from "../../Services/uber-service.service";
+import {User} from "../../User";
 
 @Component({
   selector: 'app-uber',
@@ -8,7 +9,8 @@ import {UberService} from "../../Services/uber-service.service";
 })
 export class UberComponent implements OnInit {
 
-  constructor(private uberService: UberService) { }
+  constructor(private uberService: UberService) {
+  }
 
   @Input() endLongitude: string;// = "-73.694143";
   @Input() endLatitude: string;// = "40.84100560688397";
@@ -19,27 +21,63 @@ export class UberComponent implements OnInit {
   Estimates: any;
   uberData: any;
 
-  Address: string = "22 Knoll Lane";
-  City: string = "Glen Head";
-  State: string = "NY";
-  Zip_Code: string = "11545";
+  // Address: string = "22 Knoll Lane";
+  // City: string = "Glen Head";
+  // State: string = "NY";
+  // Zip_Code: string = "11545";
+
+  Address: string = "700 Commonwealth Avenue";
+  City: string = "Boston";
+  State: string = "MA";
+  Zip_Code: string = "02215";
+
+  // @Input() Address: string;
+  // @Input() City: string;
+  // @Input() State: string;
+  // @Input() Zip_Code: string;
+
+  // Address: string = User.address;
+  // City: string = User[0].city;
+  // State: string = User[0].state;
+  // Zip_Code: string = User[0].zipcode;
 
   
+  truthValue = false;
 
-  public UberEstimates(){
+  public UberEstimates() {
     console.log(this.endLongitude);
     this.uberService.getUberEstimates(this.Address, this.City, this.State, this.Zip_Code, this.endLatitude, this.endLongitude).subscribe(uberData => {
       this.uberData = uberData;
-      this.Estimates = this.uberData.prices[0].estimate;
+      if ((Object.keys(this.uberData)[0]) == "fields") {
+        if (this.uberData.fields.start_longitude == "Distance between two points exceeds 100 miles" || this.uberData.prices[0].estimate == "" || this.uberData.prices[0].estimate == [] || this.uberData.prices[0].estimate == undefined || (this.City == undefined && this.State == undefined)) {
+          this.Estimates = "No Uber Estimates Available! Try another address."
+        }
+      }
+
+
+      else {
+        this.Estimates = this.uberData.prices[0].estimate;
+      }
+      this.truthValue = true;
+
+
     });
   };
 
-  ngOnInit() { 
-    this.UberEstimates();
+  public refresh() {
+    this.Address = "";
+    this.City = "";
+    this.State = "";
+    this.Zip_Code = "";
+    this.Estimates = "";
+    this.truthValue = false;
+
   }
 
 
-
-
-
+  ngOnInit() {
+    this.UberEstimates();
+  }
 }
+
+
